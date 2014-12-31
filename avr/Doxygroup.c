78c06c761ea135b1646467735dad7105fb32c5eb
Arjan <Arjan.van.Vught@gmail.com>
2014-12-31 18:21:02 +0100

    Doxygen

---
+++ avr/Doxygroup.c
@@ -0,0 +1,27 @@
+/**
+ * @defgroup Firmware firmware
+ * @brief AVR Atmel
+ *
+ */
+
+/**
+ * @defgroup I2C i2c
+ * @ingroup Firmware
+ * @brief TODO
+ *
+ */
+
+/**
+ * @defgroup SPI spi
+ * @ingroup Firmware
+ * @brief TODO
+ *
+ */
+
+
+/**
+ * @defgroup UART UART
+ * @ingroup Firmware
+ * @brief TODO
+ *
+ */
